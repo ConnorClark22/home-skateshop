e233caf4edd337d04b85e469501ab3770d2413fe
Connor Clark <connsect@outlook.com>
2022-07-22 16:35:23 -0400

    added hamburger menu

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// CONTACT FORM
 //Get data
 const nameInput = document.querySelector("#name");
 const email = document.querySelector("#email");
@@ -51,6 +52,27 @@ function emailIsValid(email) {
     return pattern.test(email);
 }
 
+
+
+//BURGER MENU
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navLinks');
+    const navLinks = document.querySelectorAll('.navLinks li');
+    //toggle nav
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active')
+    });
+    //animate links
+    navLinks.forEach((link, index) =>{
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`;
+        console.log(index / 7);
+   
+    });
+}
+
+navSlide();
 
 
 
